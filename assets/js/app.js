eeaeee4d2dcd0f3f28384c0f6628dd798750f750
Max Power <dbroncosfan@gmail.com>
2019-11-21 22:19:31 -0700

    Search form now creates buttons and pulls button name when clicked, changed theme to any GIF

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
 
-var animalBtns = ["Panda's", "Monkey's", "Hippo's", "Dog's"]
-for(var j = 0; j < animalBtns.length; j++) {
-    var btnCreate = $("<button>" + animalBtns[j] + "</button>")
+var topicBtns = ["Pulp Fiction", "Bart Simpson", "Futurama", "Rick Sanchez", "Mr Meeseeks"]
+for(var j = 0; j < topicBtns.length; j++) {
+    var btnCreate = $("<button>" + topicBtns[j] + "</button>")
         btnCreate.attr("type", "button");
-        btnCreate.attr("class", "btn btn-success m-1");
-        btnCreate.attr("data-animal", animalBtns[j]);
+        btnCreate.attr("class", "btn btn-success m-1 myButtons");
+        btnCreate.attr("data-topic", topicBtns[j]);
         btnCreate.appendTo("#buttonArray")
 }
 
-
-$("button").on("click", function() {
-    var animal = $(this).attr("data-animal");
+$(document).on("click", ".myButtons", function() {
+    var topic = $(this).attr("data-topic");
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        animal + "&rating=PG&api_key=Q5CeRLTcc4mA4AROS0aoH0ydGDpEXgF9&limit=10";
+        topic + "&rating=PG&api_key=Q5CeRLTcc4mA4AROS0aoH0ydGDpEXgF9&limit=10";
 
 $.ajax({
     url: queryURL,
@@ -26,20 +25,20 @@ $.ajax({
             var gifDiv = $("<div>");
             var rating = results[i].rating;
             var p = $("<p>").text("Rating: " + rating);
-            var animalImg = $("<img>");
-            animalImg.attr("src", results[i].images.fixed_height_still.url);
-            animalImg.attr("data-still", results[i].images.fixed_height_still.url);
-            animalImg.attr("data-animate", results[i].images.fixed_height.url);
-            animalImg.attr("data-state", "still");
-            animalImg.attr("class", "gif");
+            var topicImg = $("<img>");
+            topicImg.attr("src", results[i].images.fixed_height_still.url);
+            topicImg.attr("data-still", results[i].images.fixed_height_still.url);
+            topicImg.attr("data-animate", results[i].images.fixed_height.url);
+            topicImg.attr("data-state", "still");
+            topicImg.attr("class", "gif");
 
             gifDiv.prepend(p);
-            gifDiv.append(animalImg);
+            gifDiv.append(topicImg);
 
-            $("#animal-gifs-here").prepend(gifDiv);
+            $("#topic-gifs-here").prepend(gifDiv);
 
             };
-            $("body").on("click", ".gif", function() {
+            $(document).on("click", ".gif", function() {
                 var state = $(this).attr("data-state");
                 if (state === "still") {
                     $(this).attr("src", $(this).attr("data-animate"));
@@ -54,17 +53,22 @@ $.ajax({
 
     });
 
-$("#searchForm").submit(function(e) {
+    $("#searchForm").on("click", function(e) {
         e.preventDefault();
-});
+    });
 
-    $("#searchBtn").click(function() { 
-        var newBtn = $("<button>");
-        newBtn.attr("type", "button");
+    $("#addTopic").on("click", function() {
+
+        var newButton = $("<button>");
+        var newTopic = $("#searchInput").val().trim();
+        console.log(newTopic);
+        newButton.attr("type", "submit");
+        newButton.attr("class", "btn btn-success m-1 myButtons")
+        newButton.attr("data-topic", newTopic);
+        newButton.append(newTopic);
         
+        $("#buttonArray").append(newButton);
+    });
 
-        newBtn.append(animalBtns);
-
-});
 
 });
